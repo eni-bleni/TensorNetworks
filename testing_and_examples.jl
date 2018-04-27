c3b646be983f2d5bd9ef5c7ca4127255cb50f9c6
Johannes Knaute <Johannes@Johanness-MacBook-Pro.local>
2018-04-27 12:02:55 +0200

    plots for entanglement entropy added using PyPlot

--- testing_and_examples.jl
+++ testing_and_examples.jl
@@ -1,10 +1,12 @@
 using MPS
 using TEBD
-using Plots
-println("\n---------------------------------------")
+# using Plots
+using PyPlot
+println("------------------------------------")
+
 
 ## parameters for the spin chain:
-latticeSize = 10
+latticeSize = 50
 maxBondDim = 20
 d = 2
 prec = 1e-8
@@ -18,7 +20,7 @@ g = 0.0
 Jx = 1.0
 Jy = 1.0
 Jz = 1.0
-hx = 0.0
+hx = 1.0
 
 ## TEBD parameters:
 total_time = -im*10.0 # -im*total_time  for imag time evol
@@ -38,44 +40,76 @@ s0 = [0 0; 0 0]
 ################################################################################
 println("...performing ground state DMRG...")
 
-# hamiltonian = MPS.IsingMPO(latticeSize, J, h, g)
-hamiltonian = MPS.HeisenbergMPO(latticeSize, Jx, Jy, Jz, hx)
+hamiltonian = MPS.IsingMPO(latticeSize, J, h, g)
+# hamiltonian = MPS.HeisenbergMPO(latticeSize, Jx, Jy, Jz, hx)
 
 mps = MPS.randomMPS(latticeSize,d,maxBondDim)
 MPS.makeCanonical(mps)
 
-@time ground,E = MPS.DMRG(mps,hamiltonian,prec)
-println("E/N = ", E/(latticeSize-1))
-# println("entropy: ",MPS.entropy(ground,3))
+@time ground,E0 = MPS.DMRG(mps,hamiltonian,prec)
+println("E/N = ", E0/(latticeSize-1))
+
 
 # println("\n...performing excited state DMRG...")
 # @time exc,E = MPS.DMRG(mps,hamiltonian,prec,ground)
 # println("Overlap: ",MPS.MPSoverlap(exc,ground))
 
 
+################################################################################
+##                           Entanglement Entropy
+################################################################################
+println("\n...entanglement entropy...")
+
+entropy = Array{Any}(latticeSize,2)
+for i = 0:latticeSize-1
+    entropy[i+1,1] = i+1 # subsystem size
+    entropy[i+1,2] = MPS.entropy(ground,i)
+end
+ind_min = 5
+ind_max = Int(round(0.3*latticeSize))
+fit_interval = log.(entropy[ind_min:ind_max,1])
+a,b = linreg(fit_interval, entropy[ind_min:ind_max,2])
+c = 6*b
+println("central charge (if critical): ", c)
+
+figure(1)
+plot(entropy[:,1], entropy[:,2])
+xlabel("\$L\$ (subsystem size)")
+ylabel("\$S_L\$ (entanglement entropy)")
+
+figure(2)
+plot(fit_interval, a+b*fit_interval)
+plot(fit_interval, entropy[ind_min:ind_max,2])
+xlabel("\$\\ln(L)\$")
+ylabel("\$S_L\$")
+# text(fit_interval[end]*0.6, entropy[ind_max,2]*0.99, "c = %1.2f" %c)
+show()
 
 ################################################################################
 ##                                  TEBD
 ################################################################################
-println("\n...performing TEBD...")
 
-# ham = TEBD.TwoSiteIsingHamiltonian(J,h,g)
-ham = TEBD.TwoSiteHeisenbergHamiltonian(Jx,Jy,Jz,hx)
+### ATTENTION: uses not correct hamiltonian at endpoints, use quench.jl instead
 
-mps2 = MPS.randomMPS(latticeSize,d,maxBondDim)
-# mps2 = ground
-MPS.makeCanonical(mps2)
-
-println("Norm: ", MPS.MPSnorm(mps2)," Energy: ", MPS.mpoExpectation(mps2,hamiltonian))
-
-magnetization = MPS.OneSiteMPO(latticeSize, Int(round(latticeSize/2)), sx)
-expect = TEBD.time_evolve(mps2, ham, total_time, steps, maxBondDim, hamiltonian)
-
-println("Norm: ", MPS.MPSnorm(mps2)," Energy: ", MPS.mpoExpectation(mps2,hamiltonian))
-println( "E/N = ", MPS.mpoExpectation(mps2,hamiltonian)/(latticeSize-1) )
-
-
-## PLOTTING
-plot(abs.(expect[:,1]), real.(expect[:,2]), show=true)
+# println("\n...performing TEBD...")
+#
+# # ham = TEBD.TwoSiteIsingHamiltonian(J,h,g)
+# ham = TEBD.TwoSiteHeisenbergHamiltonian(Jx,Jy,Jz,hx)
+#
+# mps2 = MPS.randomMPS(latticeSize,d,maxBondDim)
+# # mps2 = ground
+# MPS.makeCanonical(mps2)
+#
+# println("Norm: ", MPS.MPSnorm(mps2)," Energy: ", MPS.mpoExpectation(mps2,hamiltonian))
+#
+# magnetization = MPS.OneSiteMPO(latticeSize, Int(round(latticeSize/2)), sx)
+# expect = TEBD.time_evolve(mps2, ham, total_time, steps, maxBondDim, hamiltonian)
+#
+# println("Norm: ", MPS.MPSnorm(mps2)," Energy: ", MPS.mpoExpectation(mps2,hamiltonian))
+# println( "E/N = ", MPS.mpoExpectation(mps2,hamiltonian)/(latticeSize-1) )
+#
+#
+# ## PLOTTING
+# plot(abs.(expect[:,1]), real.(expect[:,2]), show=true)
 
 ;
