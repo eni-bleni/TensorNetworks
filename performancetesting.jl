d9a9462c9a9ff898cf29e92cf6ef16f3e278d965
Viktor Svensson <svensson.viktor.91@gmail.com>
2018-07-19 15:42:01 +0200

    some performance testing

---
+++ performancetesting.jl
@@ -0,0 +1,17 @@
+using MPS
+
+## We know how certain operations should scale with
+## bond dimension and system size. We should test this.
+time = []
+Ds = []
+mpo = MPS.IsingMPO(10,1,1,0)
+for D = 1:7
+    # mps = MPS.randomMPS(10,2,)
+    # @time MPS.MPSnorm(mps)
+    println(D)
+    push!(time, @elapsed MPS.traceMPOprod(mpo,mpo,2))
+    push!(Ds,first(size(mpo[5])))
+    mpo = MPS.addmpos(mpo,mpo,false)
+end
+a,b = linreg(log.(Ds),log.(time))
+println(b)
